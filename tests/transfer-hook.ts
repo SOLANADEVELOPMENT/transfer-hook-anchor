c4935af5b75ac7814655369519761092ad2376d2
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-12-11 14:49:02 +0700

    Update mint address

--- tests/transfer-hook.ts
+++ tests/transfer-hook.ts
@@ -140,6 +140,8 @@ describe("transfer-hook", () => {
     );
 
     console.log(`Transaction Signature: ${txSig}`);
+    // console.log(`Transaction: https://explorer.solana.com/address/${txSig}?cluster=devnet`);
+    console.log(`Mint address: https://explorer.solana.com/address/${mint.publicKey.toBase58()}?cluster=devnet`);
   });
 
   // Account to store extra accounts required by the transfer hook instruction
